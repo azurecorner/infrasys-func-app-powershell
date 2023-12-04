2cce8d8e7e00210bf8b83ea00bd65ca5eaa7de37
Gora LEYE <leyegora@yahoo.fr>
2023-12-04 11:47:57 +0100

    deploy peerings

---
+++ scripts/virtualNetworkPeering.ps1
@@ -0,0 +1,26 @@
+$hubVirtualNetworkName="vnet-infrasys-hub-shared-frace-nonprod"
+$hubResourceGroupName="rg-infrasys-shared-nonprod"
+
+$spokeVirtualNetworkName="VNET-SYSTEME-FRACE-001"
+$spokeResourceGroupName="RG-SYSTEME-FRACE-001-SANDBOX"
+
+$hubVirtualNetwork =  Get-AzVirtualNetwork -Name $hubVirtualNetworkName -ResourceGroupName $hubResourceGroupName 
+
+$spokeVirtualNetwork =  Get-AzVirtualNetwork -Name $spokeVirtualNetworkName -ResourceGroupName $spokeResourceGroupName 
+
+Add-AzVirtualNetworkPeering `
+  -Name myVirtualNetwork1-myVirtualNetwork2 `
+  -VirtualNetwork $hubVirtualNetwork `
+  -RemoteVirtualNetworkId $spokeVirtualNetwork.Id
+
+
+  Add-AzVirtualNetworkPeering `
+  -Name myVirtualNetwork2-myVirtualNetwork1 `
+  -VirtualNetwork $spokeVirtualNetwork `
+  -RemoteVirtualNetworkId $hubVirtualNetwork.Id
+
+
+  Get-AzVirtualNetworkPeering `
+  -ResourceGroupName myResourceGroup `
+  -VirtualNetworkName myVirtualNetwork1 `
+  | Select-Object PeeringState
