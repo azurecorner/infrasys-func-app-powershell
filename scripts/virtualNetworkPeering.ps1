983e51fe167b01ec06a2d03e980fa18cf034c90b
Gora LEYE <leyegora@yahoo.fr>
2023-12-05 10:26:04 +0100

    fix

--- scripts/virtualNetworkPeering.ps1
+++ scripts/virtualNetworkPeering.ps1
@@ -1,11 +1,9 @@
 $hubVirtualNetworkName="vnet-infrasys-hub-shared-frace-nonprod"
 $hubResourceGroupName="rg-infrasys-shared-nonprod"
-
 $spokeVirtualNetworkName="VNET-SYSTEME-FRACE-001"
 $spokeResourceGroupName="RG-SYSTEME-FRACE-001-SANDBOX"
 
 $hubVirtualNetwork =  Get-AzVirtualNetwork -Name $hubVirtualNetworkName -ResourceGroupName $hubResourceGroupName 
-
 $spokeVirtualNetwork =  Get-AzVirtualNetwork -Name $spokeVirtualNetworkName -ResourceGroupName $spokeResourceGroupName 
 
 $peering = Get-AzVirtualNetworkPeering  `
@@ -16,10 +14,13 @@ $peering = Get-AzVirtualNetworkPeering  `
 Write-Host "peering hub  = $peering"
 
 if($null -eq $peering ) {
+    Write-Host -ForegroundColor Green "creating virtual network peering hub-to-spoke $($hubVirtualNetwork.Name) and $($spokeVirtualNetwork.Name) "
     Add-AzVirtualNetworkPeering `
       -Name "hub-to-spoke" `
       -VirtualNetwork $hubVirtualNetwork `
       -RemoteVirtualNetworkId $spokeVirtualNetwork.Id
+}else {
+  Write-Host -ForegroundColor Yellow "virtual network peering hub-to-spoke already exist"
 }
 
 $peering = Get-AzVirtualNetworkPeering  `
@@ -30,8 +31,11 @@ $peering = Get-AzVirtualNetworkPeering  `
 Write-Host "peering spoke  = $peering"
 
 if($null -eq $peering ) {
+  Write-Host -ForegroundColor Green "creating virtual network peering spoke-to-hub $($hubVirtualNetwork.Name) and $($spokeVirtualNetwork.Name) "
   Add-AzVirtualNetworkPeering `
   -Name "spoke-to-hub" `
   -VirtualNetwork $spokeVirtualNetwork `
   -RemoteVirtualNetworkId $hubVirtualNetwork.Id
+}else {
+  Write-Host -ForegroundColor Yellow "virtual network peering spoke-to-hub already exist"
 }
