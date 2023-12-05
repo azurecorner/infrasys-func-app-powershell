fabe803b3c7be54702ace78d49a5a519c14e0dfd
Gora LEYE <leyegora@yahoo.fr>
2023-12-05 10:37:04 +0100

    fix it

--- scripts/virtualNetworkPeering.ps1
+++ scripts/virtualNetworkPeering.ps1
@@ -6,36 +6,34 @@ $spokeResourceGroupName="RG-SYSTEME-FRACE-001-SANDBOX"
 $hubVirtualNetwork =  Get-AzVirtualNetwork -Name $hubVirtualNetworkName -ResourceGroupName $hubResourceGroupName 
 $spokeVirtualNetwork =  Get-AzVirtualNetwork -Name $spokeVirtualNetworkName -ResourceGroupName $spokeResourceGroupName 
 
-$peering = Get-AzVirtualNetworkPeering  `
--Name "hub-to-spoke"  `
--VirtualNetworkName $hubVirtualNetwork  `
--ResourceGroupName $hubResourceGroupName -ErrorAction SilentlyContinue
+$peering = Get-AzVirtualNetworkPeering -Name "hub-to-spoke" -VirtualNetworkName $hubVirtualNetwork -ResourceGroupName $hubResourceGroupName -ErrorAction SilentlyContinue
 
 Write-Host "peering hub  = $peering"
 
 if($null -eq $peering ) {
     Write-Host -ForegroundColor Green "creating virtual network peering hub-to-spoke $($hubVirtualNetwork.Name) and $($spokeVirtualNetwork.Name) "
-    Add-AzVirtualNetworkPeering `
-      -Name "hub-to-spoke" `
-      -VirtualNetwork $hubVirtualNetwork `
-      -RemoteVirtualNetworkId $spokeVirtualNetwork.Id -Force
-}else {
-  Write-Host -ForegroundColor Yellow "virtual network peering hub-to-spoke already exist"
+    $hubPeeringParams = @{
+        'Name' = 'hub-to-spoke'
+        'VirtualNetwork' = $hubVirtualNetwork
+        'RemoteVirtualNetworkId' = $spokeVirtualNetwork.Id
+    }
+    Add-AzVirtualNetworkPeering @hubPeeringParams
+} else {
+    Write-Host -ForegroundColor Yellow "virtual network peering hub-to-spoke already exists"
 }
 
-$peering = Get-AzVirtualNetworkPeering  `
--Name "spoke-to-hub"  `
--VirtualNetworkName $spokeVirtualNetwork  `
--ResourceGroupName $spokeResourceGroupName -ErrorAction SilentlyContinue
+$peering = Get-AzVirtualNetworkPeering -Name "spoke-to-hub" -VirtualNetworkName $spokeVirtualNetwork -ResourceGroupName $spokeResourceGroupName -ErrorAction SilentlyContinue
 
 Write-Host "peering spoke  = $peering"
 
 if($null -eq $peering ) {
-  Write-Host -ForegroundColor Green "creating virtual network peering spoke-to-hub $($hubVirtualNetwork.Name) and $($spokeVirtualNetwork.Name) "
-  Add-AzVirtualNetworkPeering `
-  -Name "spoke-to-hub" `
-  -VirtualNetwork $spokeVirtualNetwork `
-  -RemoteVirtualNetworkId $hubVirtualNetwork.Id -Force
-}else {
-  Write-Host -ForegroundColor Yellow "virtual network peering spoke-to-hub already exist"
+    Write-Host -ForegroundColor Green "creating virtual network peering spoke-to-hub $($spokeVirtualNetwork.Name) and $($hubVirtualNetwork.Name) "
+    $spokePeeringParams = @{
+        'Name' = 'spoke-to-hub'
+        'VirtualNetwork' = $spokeVirtualNetwork
+        'RemoteVirtualNetworkId' = $hubVirtualNetwork.Id
+    }
+    Add-AzVirtualNetworkPeering @spokePeeringParams
+} else {
+    Write-Host -ForegroundColor Yellow "virtual network peering spoke-to-hub already exists"
 }
