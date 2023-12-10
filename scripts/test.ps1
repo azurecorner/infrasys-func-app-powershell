a0a71ad75d7bd3a1fa9b148a2d591381c34bc976
Gora LEYE <tocane.technologies@gmail.com>
2023-12-10 13:16:21 +0100

    fix

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -2,8 +2,8 @@
 
 # Variable block
 
-$location = "francecentral"
-$resourceGroup = "RG-SYSTEME-FRACE-001-TEST"
+$resourceGroupLocation = "francecentral"
+$resourceGroupName = "RG-SYSTEME-FRACE-001-TEST"
 $appServicePlan = "funcswapnsgtrafficxyzasp"
 $functionApp = "funcswapnsgtrafficxyz"
 $storage = "st$functionApp"
@@ -12,37 +12,37 @@ $skuPlan = "B1"
 $functionsVersion = "4"
 
 # Create a resource group
-# Write-Host "Creating $resourceGroup in $location..."
-# New-AzResourceGroup -Name $resourceGroup -Location $location -Tag $tag
+# Write-Host "Creating $resourceGroupName in $location..."
+# New-AzResourceGroup -Name $resourceGroupName -Location $location -Tag $tag
 
-$storageAcc=Get-AzStorageAccount -ResourceGroupName $resourceGroup -Name $storage -ErrorVariable notPresent -ErrorAction SilentlyContinue
+$storageAcc=Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storage -ErrorVariable notPresent -ErrorAction SilentlyContinue
 if($notPresent){ 
     Write-Host "Creating $storage"
     # Create an Azure storage account in the resource group.
     Write-Host "Creating $storage"
-    $storageAcc=New-AzStorageAccount -Name $storage -Location $location -ResourceGroupName $resourceGroup -SkuName $skuStorage
+    $storageAcc=New-AzStorageAccount -Name $storage -Location $resourceGroupLocation -ResourceGroupName $resourceGroup -SkuName $skuStorage
 
 }
 else {
     Write-Host -ForegroundColor Yellow "storage account already  exist"
-    Write-Host  -ForegroundColor Magenta "using storage account $storageAccountName on location $location  "
+    Write-Host  -ForegroundColor Magenta "using storage account $storageAccountName on location $resourceGroupLocation  "
 
 }
 
 
 # Create an App Service plan
 Write-Host "Creating $appServicePlan"
-New-AzFunctionAppPlan -Name $appServicePlan -ResourceGroupName $resourceGroup -Location $location -Sku $skuPlan -WorkerType Windows
+New-AzFunctionAppPlan -Name $appServicePlan -ResourceGroupName $resourceGroup -Location $resourceGroupLocation -Sku $skuPlan -WorkerType Windows
 
 
-$function = Get-AzFunctionApp -ResourceGroupName $resourceGroup -Name $functionApp  -ErrorAction SilentlyContinue
+$function = Get-AzFunctionApp -ResourceGroupName $resourceGroupName -Name $functionApp  -ErrorAction SilentlyContinue
 
 Write-Host "notPresent = $function"
 Write-Host "Creating function app $function"
 if($null -eq $function){ 
     # Create a Function App
     Write-Host "Creating $function"
-    $function=New-AzFunctionApp -Name $functionApp -StorageAccountName $storage -PlanName $appServicePlan -ResourceGroupName $resourceGroup -Runtime PowerShell -FunctionsVersion $functionsVersion
+    $function=New-AzFunctionApp -Name $functionApp -StorageAccountName $storage -PlanName $appServicePlan -ResourceGroupName $resourceGroupName -Runtime PowerShell -FunctionsVersion $functionsVersion
 
  }
 
