22f2c722a046276e0a213ecd22a62ea23756c60a
Gora LEYE <tocane.technologies@gmail.com>
2023-12-10 12:43:53 +0100

    FIX

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,13 +1,13 @@
 # Function app and storage account names must be unique.
 
 # Variable block
-$randomIdentifier = Get-Random
+
 $location = "francecentral"
-$resourceGroup = "RG-SYSTEME-FRACE-001-SANDBOX"
-$tag = @{script = "create-function-app-app-service-plan"}
-$storage = "msdocsaccount$randomIdentifier"
-$appServicePlan = "msdocs-app-service-plan-$randomIdentifier"
-$functionApp = "msdocs-serverless-function-$randomIdentifier"
+$resourceGroup = "RG-SYSTEME-FRACE-001-TEST"
+
+$storage = "st$functionAppName"
+$appServicePlan = "funcswapnsgtrafficxyzasp"
+$functionApp = "funcswapnsgtrafficxyz"
 $skuStorage = "Standard_LRS"
 $skuPlan = "B1"
 $functionsVersion = "4"
@@ -16,14 +16,33 @@ $functionsVersion = "4"
 # Write-Host "Creating $resourceGroup in $location..."
 # New-AzResourceGroup -Name $resourceGroup -Location $location -Tag $tag
 
-# Create an Azure storage account in the resource group.
-Write-Host "Creating $storage"
-New-AzStorageAccount -Name $storage -Location $location -ResourceGroupName $resourceGroup -SkuName $skuStorage
+$storageAcc=Get-AzStorageAccount -ResourceGroupName $resourceGroup -Name $storage -ErrorVariable notPresent -ErrorAction SilentlyContinue
+if($notPresent){ 
+    Write-Host "Creating $storage"
+    # Create an Azure storage account in the resource group.
+    Write-Host "Creating $storage"
+    $storageAcc=New-AzStorageAccount -Name $storage -Location $location -ResourceGroupName $resourceGroup -SkuName $skuStorage
+
+}
+else {
+    Write-Host -ForegroundColor Yellow "storage account already  exist"
+    Write-Host  -ForegroundColor Magenta "using storage account $storageAccountName on location $location  "
+
+}
+
 
 # Create an App Service plan
 Write-Host "Creating $appServicePlan"
 New-AzFunctionAppPlan -Name $appServicePlan -ResourceGroupName $resourceGroup -Location $location -Sku $skuPlan -WorkerType Windows
 
-# Create a Function App
-Write-Host "Creating $functionApp"
-New-AzFunctionApp -Name $functionApp -StorageAccountName $storage -PlanName $appServicePlan -ResourceGroupName $resourceGroup -Runtime PowerShell -FunctionsVersion $functionsVersion
+
+$function = Get-AzFunctionApp -ResourceGroupName $resourceGroup -Name $functionApp  -ErrorAction SilentlyContinue
+
+Write-Host "notPresent = $function"
+Write-Host "Creating function app $function"
+if($null -eq $function){ 
+    # Create a Function App
+    Write-Host "Creating $function"
+    New-AzFunctionApp -Name $functionApp -StorageAccountName $storage -PlanName $appServicePlan -ResourceGroupName $resourceGroup -Runtime PowerShell -FunctionsVersion $functionsVersion
+
+ }
